2f76f994db5ad1078e2c4c597f8d65cd0295d355
Sean-O-99 <sean.osullivan015@gmail.com>
2024-10-04 23:43:23 +1000

    Nutrition breakdown function nearly complete

--- Milestone_2/MILESTONE2/breakdownDialog.py
+++ Milestone_2/MILESTONE2/breakdownDialog.py
@@ -20,17 +20,27 @@ _ = gettext.gettext
 class MyDialog2 ( wx.Dialog ):
 
     def __init__( self, parent ):
-        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 485,477 ), style = wx.DEFAULT_DIALOG_STYLE )
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 800,500 ), style = wx.DEFAULT_DIALOG_STYLE )
 
         self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
-        bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+        bSizer4 = wx.BoxSizer( wx.VERTICAL )
+
+        bSizer5 = wx.BoxSizer( wx.HORIZONTAL )
+
+        self.m_button6 = wx.Button( self, wx.ID_ANY, _(u"Plot"), wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer5.Add( self.m_button6, 0, wx.ALL, 5 )
+
+        self.m_staticText9 = wx.StaticText( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+        self.m_staticText9.Wrap( -1 )
+
+        bSizer5.Add( self.m_staticText9, 0, wx.ALL, 5 )
+
+
+        bSizer4.Add( bSizer5, 0, wx.EXPAND, 0 )
 
         self.m_panel1 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer4.Add( self.m_panel1, 1, wx.EXPAND |wx.ALL, 5 )
-
-        self.m_panel2 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        bSizer4.Add( self.m_panel2, 1, wx.EXPAND |wx.ALL, 5 )
+        bSizer4.Add( self.m_panel1, 1, wx.EXPAND |wx.ALL, 0 )
 
 
         self.SetSizer( bSizer4 )
@@ -38,7 +48,14 @@ class MyDialog2 ( wx.Dialog ):
 
         self.Centre( wx.BOTH )
 
+        # Connect Events
+        self.m_button6.Bind( wx.EVT_BUTTON, self.clickOnPlot )
+
     def __del__( self ):
         pass
 
+
+    # Virtual event handlers, override them in your derived class
+    def clickOnPlot( self, event ):
+        event.Skip()
 
--- Milestone_2/MILESTONE2/mainPageFunctionality.py
+++ Milestone_2/MILESTONE2/mainPageFunctionality.py
@@ -1,9 +1,13 @@
 import wx
 import wx.grid
 import pandas as pd
+from wx.lib.agw.aui import right
+from wx.lib.masked import value
+
 from mainPage import MyFrame2 as MyFrame2
 from breakdownDialogFunctionality import NutritionalDialog
 from filterDialogFunctionality import FilterDialog
+import matplotlib.pyplot as plt
 
 class DataTable(wx.grid.GridTableBase):
     def __init__(self, data=None):
@@ -57,10 +61,65 @@ class FoodDataTable(MyFrame2):
     def on_cell_click(self, event):
         row = event.GetRow()
         food_item = self.m_grid1.GetCellValue(row, 0)
-        nutritional_info = self.df.iloc[row].to_dict()
+        df_row = self.df[self.df["food"] == food_item]
+        nutritional_info = df_row.iloc[0].to_dict()
+        nutritional_values = {key: value for key, value in nutritional_info.items() if key !="food"}
 
-        dialog = NutritionalDialog(None, food_item, nutritional_info)
+        nutritional_items = list(nutritional_values.items())
+        fat = nutritional_items[1]
+        carbs = nutritional_items[5]
+        protein = nutritional_items[7]
+        vitamin_a = nutritional_items[12]
+        vitamin_b6 = nutritional_items[19]
+        vitamin_b12 = nutritional_items[15]
+        vitamin_c = nutritional_items[20]
+        vitamin_d = nutritional_items[21]
+        calcium = nutritional_items[24]
+        iron = nutritional_items[26]
+        magnesium = nutritional_items[27]
+        potassium = nutritional_items[30]
+        zinc = nutritional_items[32]
+
+        macronutrients = {
+            fat[0] : fat[1],
+            carbs[0] : carbs[1],
+            protein[0] : protein[1]
+        }
+
+        micronutrients = {
+            vitamin_a[0]: vitamin_a[1],
+            vitamin_b6[0]: vitamin_b6[1],
+            vitamin_b12[0]: vitamin_b12[1],
+            vitamin_c[0]: vitamin_c[1],
+            vitamin_d[0] : vitamin_d[1],
+            calcium[0]: calcium[1],
+            iron[0]: iron[1],
+            magnesium[0]: magnesium[1],
+            potassium[0]: potassium[1],
+            zinc[0]: zinc[1],
+        }
+
+        macro_categories = list(macronutrients.keys())
+        macro_sizes = list(macronutrients.values())
+
+        micro_categories = list(micronutrients.keys())
+        micro_sizes = list(micronutrients.values())
+
+        fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+        ax1, ax2 = axes
+        ax1.pie(macro_sizes, labels=macro_categories, shadow=True, autopct="%1.1f%%")
+        ax1.set_title("Macronutrients")
+
+        plt.xticks(rotation=45, ha="right")
+        ax2.bar(micro_categories, micro_sizes)
+        ax2.set_title("Micronutrients")
+
+        plt.tight_layout()
+
+        dialog = NutritionalDialog(None, food_item, fig)
         dialog.ShowModal()
+
+        # return food_item, macronutrients
 
     def open_filter_dialog(self, event):
         dialog = FilterDialog(None)
